292000d896790579106bfa72358d597358a86fc5
Peter Caisse <pcaisse@gmail.com>
2021-04-26 16:19:17 -0400

    Configure project to build with TypeScript
    This uses `ts-loader` on top of `babel-loader`. It's possible to forgo
    using `ts-loader`, opting instead to use `@babel/preset-typescript` but
    that comes at the cost of type safety when building (see
    https://evanlouie.github.io/posts/typescript-babel-preset-typescript-ts-loader).

--- webpack.config.js
+++ webpack.config.js
@@ -7,47 +7,57 @@ const env = require('yargs').argv.env; // use --env with webpack 2
 
 let libraryName = 'loam';
 
-let plugins = [], outputFile;
+let plugins = [],
+    outputFile;
 
 if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = '[name].min.js';
+    plugins.push(new UglifyJsPlugin({ minimize: true }));
+    outputFile = '[name].min.js';
 } else {
-  outputFile = '[name].js';
+    outputFile = '[name].js';
 }
 
 const config = {
-  entry: {
-      loam: __dirname + '/src/index.js',
-      'loam-worker': __dirname + '/src/worker.js'
-  },
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/lib',
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    rules: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules|bower_components)/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    modules: [path.resolve('./node_modules'), path.resolve('./src')],
-    extensions: ['.json', '.js']
-  },
-  plugins: plugins
+    entry: {
+        loam: __dirname + '/src/index.ts',
+        'loam-worker': __dirname + '/src/worker.js'
+    },
+    devtool: 'source-map',
+    output: {
+        path: __dirname + '/lib',
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true,
+                            presets: [['@babel/preset-env', { targets: { node: '8' } }]]
+                        }
+                    },
+                    'ts-loader'
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        modules: [path.resolve('./node_modules'), path.resolve('./src')],
+        extensions: ['.json', '.js', '.ts']
+    },
+    plugins: plugins
 };
 
 module.exports = config;
